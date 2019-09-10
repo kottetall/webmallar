e6fc38e687e1e52b690416cae211ad151e06bff3
Alexander Hedling <42749900+kottetall@users.noreply.github.com>
2019-09-10 16:58:12 +0200

    Första versionen klar

---
+++ webmall.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+# Vägen dit nya mappen ska ligga
+MAL="/c/Users/Taike/Documents/Programmering/web"
+
+# där mallen finns
+MALLPLATS="/c/Users/Taike/Documents/Programmering/web/mall"
+
+# Namnet på nya projektet/mappen
+read -p "Vilket namn har projektet? (INGA MELLANRUM I NAMNET) " PROJEKT
+
+# slår ihop förvald målmapp med namnet på nya projektet
+PROJEKTMAL="$MAL/$PROJEKT"
+echo $PROJEKTMAL
+
+# Kollar om mappen redan finns, om så är fallet händer inget
+if [ -d $PROJEKTMAL ]
+then 
+    echo "'$PROJEKT' finns redan och mappen/filerna har inte ersatts"
+else 
+    cp -r $MALLPLATS $PROJEKTMAL
+    echo "'$PROJEKT' har skapats"
+
+# Initierar GIT och gör en första commit med de filer som finns i mallen
+    cd $PROJEKTMAL
+    git init
+    git add .
+    git commit -m "Projektet påbörjat"
+
+fi
+
